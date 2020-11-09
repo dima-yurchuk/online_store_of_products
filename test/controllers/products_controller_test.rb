3cb48264e60f0c359c7487ea121295dc80599dea
dima-yurchuk <dimayurchuk20@gmail.com>
2020-11-09 21:04:19 +0200

    lab 2, card user

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
-    @title = "The Great Book #{rand(1000)}"
+    @title = "Сік Сандора, Вишня, 0.95л #{rand(1000)}"
   end
 
   test "should get index" do
@@ -51,7 +51,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     }
     assert_redirected_to product_url(@product)
   end
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+      end
 
+    assert_redirected_to products_url
+  end
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
